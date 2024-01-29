20ba9951975758ed1516a7c9187e19f5c8551b28
DavigJ <david.j.jung@gmail.com>
2024-01-29 11:19:23 -0500

    actual functional copperative compat

---
+++ src/main/java/com/davigj/change_of_cart/core/other/CCItemTags.java
@@ -0,0 +1,12 @@
+package com.davigj.change_of_cart.core.other;
+
+import com.teamabnormals.blueprint.core.util.TagUtil;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+public class CCItemTags {
+    public static final TagKey<Item> WAX_INDICATORS = itemTag("wax_indicators");
+    private static TagKey<Item> itemTag(String name) {
+        return TagUtil.itemTag("copperative", name);
+    }
+}
