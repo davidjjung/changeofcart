a34858b70f370647dc0ec801a610c7c2b8a553d3
DavigJ <david.j.jung@gmail.com>
2024-01-29 09:08:13 -0500

    copperative compat

--- src/main/java/com/davigj/change_of_cart/core/other/CCEvents.java
+++ src/main/java/com/davigj/change_of_cart/core/other/CCEvents.java
@@ -6,7 +6,6 @@ import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
@@ -67,5 +66,4 @@ public class CCEvents {
             }
         }
     }
-
 }
