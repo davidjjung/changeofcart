7015cc8751e043f2799bd478f3ab1cafc5aadf67
DavigJ <david.j.jung@gmail.com>
2025-01-02 11:01:09 +0400

    clean up friction mixin

--- src/main/java/com/davigj/change_of_cart/core/mixin/AbstractMinecartMixin.java
+++ src/main/java/com/davigj/change_of_cart/core/mixin/AbstractMinecartMixin.java
@@ -4,6 +4,9 @@ import com.davigj.change_of_cart.core.CCConfig;
 import com.davigj.change_of_cart.core.ChangeOfCart;
 import com.davigj.change_of_cart.core.other.CCBlockTags;
 import com.davigj.change_of_cart.core.other.CCItemTags;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.teamabnormals.blueprint.common.world.storage.tracking.TrackedDataManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
@@ -17,33 +20,29 @@ import net.minecraftforge.fml.ModList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractMinecart.class)
 public class AbstractMinecartMixin {
 
-    @Inject(method = "applyNaturalSlowdown", at = @At("HEAD"), cancellable = true)
-    private void applyNaturalSlowdown(CallbackInfo ci) {
+    @ModifyVariable(method = "applyNaturalSlowdown", at = @At(value = "STORE", ordinal = 0), name = "d0")
+    private double modifySlowdownFactor(double d0) {
         AbstractMinecart cart = (AbstractMinecart) (Object) this;
         double frictionBonus = 0;
         if (cart.level().getBlockState(cart.blockPosition().below()).is(CCBlockTags.RAIL_BEDDING)) {
-            frictionBonus += Mth.clamp(CCConfig.COMMON.railBeddingBonus.get() , 0, 10) * 0.1 * .003;
+            frictionBonus += CCConfig.COMMON.railBeddingBonus.get() * 0.1 * .003;
         }
         if (TrackedDataManager.INSTANCE.getValue(cart, ChangeOfCart.WAXED)) {
-            frictionBonus += Mth.clamp(CCConfig.COMMON.railBeddingBonus.get() , 0, 10) * 0.1 * .003;
+            frictionBonus += CCConfig.COMMON.waxFrictionBonus.get() * 0.1 * .003;
         }
         if (frictionBonus != 0) {
             frictionBonus = Mth.clamp(frictionBonus, 0, 0.003);
-            double d0 = cart.isVehicle() ? 0.997D + frictionBonus : 0.96D + (frictionBonus * 10);
-            Vec3 vec3 = cart.getDeltaMovement();
-            vec3 = vec3.multiply(d0, 0.0D, d0);
-            if (cart.isInWater()) {
-                vec3 = vec3.scale((double)0.95F);
-            }
-            cart.setDeltaMovement(vec3);
-            ci.cancel();
+            d0 = cart.isVehicle() ? 0.997D + frictionBonus : 0.96D + (frictionBonus * 10);
         }
+        return d0;
     }
+
 
     // Copperative compat
     @Inject(method = "tick", at = @At("HEAD"))
